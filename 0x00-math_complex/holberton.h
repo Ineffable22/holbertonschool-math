dab9adb0b3e1a86ab03934b91a468eaaeae82779
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-11-11 02:57:36 -0500

    Add: 4-addition file and holberton.h(new prototype)

--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -20,5 +20,6 @@ void display_complex_number(complex c);
 complex conjugate(complex c);
 double modulus(complex c);
 double argument(complex c);
+void addition(complex c1, complex c2, complex *c3);
 
 #endif
