3544f7300a7c80bb0beae3c20881969acbe2ac86
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-11-11 21:04:16 -0500

    7-division.c update: The pow function is replaced by a simple multiplication

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -9,8 +9,8 @@
 void division(complex c1, complex c2, complex *c3)
 {
 	c3->re = ((c1.re * c2.re) + (c1.im * c2.im));
-	c3->re /= (pow(c2.re, 2) + pow(c2.im, 2));
+	c3->re /= ((c2.re * c2.re) + (c2.im * c2.im));
 
 	c3->im = ((c1.re * -c2.im) + (c1.im * c2.re));
-	c3->im /= (pow(c2.re, 2) + pow(c2.im, 2));
+	c3->im /= ((c2.re * c2.re) + (c2.im * c2.im));
 }
