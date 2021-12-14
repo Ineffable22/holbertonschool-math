18cfe41819b2de95dd50606f1206ab014cea31c0
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-12-13 21:59:27 -0500

    Add: rectangle.h 0-rectangle.c files

---
+++ 0x02-math_integrals_and_ode/0-rectangle.c
@@ -0,0 +1,44 @@
+#include "rectangle.h"
+#include <stdio.h>
+/**
+ * rectangle_method - Calculates with rectangle method.
+ * @a: first number.
+ * @b: second number.
+ * @steps: number of steps.
+ *
+ * Return: The aproximation of the Integral.
+ */
+double rectangle_method(double a, double b, int steps)
+{
+	/* b = 1   a = 0 */
+	double height = 0;
+	double width = ((b - a) / steps);
+	double midpoint = width;
+	double result = 0;
+	int i = 0;
+
+	while (i <= steps)
+	{
+		height = finding_height(midpoint);
+		midpoint += width;
+		result += (width * height);
+		i++;
+	}
+	return (result);
+}
+
+/**
+ * finding_height - finds the height.
+ * @value: value of the function.
+ *
+ * Return: The height.
+ */
+double finding_height(double value)
+{
+	double result = 0;
+	double argument = 0;
+
+	argument = 1 + (value * value);
+	result = 1 / argument;
+	return (result);
+}
---
+++ 0x02-math_integrals_and_ode/rectangle.h
@@ -0,0 +1,10 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include <stdio.h>
+#include <math.h>
+
+double rectangle_method(double a, double b, int steps);
+double finding_height(double value);
+
+#endif
